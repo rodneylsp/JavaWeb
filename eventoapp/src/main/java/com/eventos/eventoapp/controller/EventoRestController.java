07580fa35d2c1dc53bc71ffcc9f523b79d4e02ef
rodney <rodney@inspiron-7560>
2019-09-29 22:23:12 +0100

    Adicionando resposta JSON ao detalhe do evento

---
+++ eventoapp/src/main/java/com/eventos/eventoapp/controller/EventoRestController.java
@@ -0,0 +1,34 @@
+package com.eventos.eventoapp.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.eventos.eventoapp.model.Evento;
+import com.eventos.eventoapp.repository.EventoRepository;
+
+@RestController
+public class EventoRestController {
+
+	@Autowired
+	private EventoRepository repository;
+
+	@RequestMapping("/eventos/{id}")
+	public ResponseEntity<Evento> getEventoById(@PathVariable("id") Long id) {
+
+		if (!id.equals(0L)) {
+
+			Optional<Evento> evento = repository.findById(id);
+			if (!evento.isEmpty()) {
+				return new ResponseEntity<Evento>(evento.get(), HttpStatus.OK);
+			}
+		}
+
+		return new ResponseEntity<Evento>(HttpStatus.NOT_FOUND);
+	}
+}
